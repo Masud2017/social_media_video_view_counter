005c2b31ff1b01e37c010bd71369cb2bb0262fd8
Masud karim <msmasud578@gmail.com>
2023-02-01 14:05:51 +0600

    updated the models

--- src/models/Models.py
+++ src/models/Models.py
@@ -7,15 +7,15 @@ db = SQLAlchemy()
 
 class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String,nullable = False)
-    email = db.Column(db.String, unique=True, nullable=False)
-    password = db.Column(db.String,nullable = False)
+    name = db.Column(db.String(30),nullable = False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200),nullable = False)
     urls = db.relationship("Url",backref="user",cascade="all,delete")
 
 class Url(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    url = db.Column(db.String,nullable = False)
-    platform = db.Column(db.String, nullable = False)
+    url = db.Column(db.String(1000),nullable = False)
+    platform = db.Column(db.String(20), nullable = False)
     view_count = db.Column(db.Integer,nullable = False)
 
