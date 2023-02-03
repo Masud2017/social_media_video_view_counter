e7a43748839cec9f83ed0f0fdfc62a07768a6ea3
Masud karim <msmasud578@gmail.com>
2023-02-03 15:32:11 +0600

    updated the stuff

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,9 @@
 from src.main import app
+from src.main import socket_
+
 import os
 
 if __name__ == "__main__":
     port = os.environ.get("PORT",5000)
-    app.run(debug=False,host="0.0.0.0",port=port)
+    socket_.run(app,debug=False,host="0.0.0.0",port=port)
 #$
