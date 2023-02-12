464bdd3208e8ad39b24303c1730cef83041adbc2
Masud karim <msmasud578@gmail.com>
2023-02-12 11:47:51 +0600

    tweaked

--- src/main.py
+++ src/main.py
@@ -82,7 +82,7 @@ def update_url_list():
         
 schedular = APScheduler()
 schedular.init_app(app)
-schedular.add_job(func=update_url_list,trigger='interval',hours=1,id='update_url_list')
+schedular.add_job(func=update_url_list,trigger='interval',minutes=25,id='update_url_list')
 schedular.start()
 atexit.register(lambda: schedular.shutdown())
 
